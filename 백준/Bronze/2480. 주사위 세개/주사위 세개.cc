655e3e1937a2b33e8bf3c01543d2a55fdc99ca9d
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-06-21 13:30:12 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    
+    cin >> A >> B >> C;
+    
+    if(A==B && B==C && A==C)
+    {
+        cout << 10000+A*1000 << endl;
+    }
+    else if(A==B || A==C)
+    {
+        cout << 1000+A*100 << endl;
+    }
+    else if(B==C)
+        cout << 1000+B*100 << endl;
+    else
+    {
+        if(A>B)
+        {
+            if(A>C)
+            {
+                cout << A*100 << endl;
+            }
+            else
+            {
+                cout << C*100 << endl;
+            }
+        }
+        else
+        {
+            if(B>C)
+            {
+                cout << B*100 << endl;
+            }
+            else
+            {
+                cout << C*100 << endl;
+            }
+        }
+    }
+}
