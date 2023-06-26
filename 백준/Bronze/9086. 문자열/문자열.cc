db7059f8dba3efabde74f966ee38a0a14cef4bf4
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-06-26 12:30:25 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    string S;
+    cin>>T;
+    for(int i = 0; i < T; i++)
+    {
+        cin >> S;
+        cout << S[0] << S[S.length()-1] << endl;
+    }
+    
+}
