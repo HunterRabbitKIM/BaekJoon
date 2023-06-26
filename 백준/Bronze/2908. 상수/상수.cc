ae88e9e598f12792ff6b9ea2797d615cc1402097
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-06-26 20:23:39 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    
+    cin >> a >> b;
+    
+    reverse(a.begin(),a.end());
+    reverse(b.begin(),b.end());
+    
+    if(a>b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
+}
