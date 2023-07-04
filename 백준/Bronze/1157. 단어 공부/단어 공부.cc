088426956089f084fb48f2fb1df22e69d6a9aa4b
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-07-04 15:35:31 +0900

    [Bronze I] Title: 단어 공부, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -1,33 +1,50 @@
 #include <iostream>
-
 using namespace std;
 
-int alpha[26], cnt =0;
-string input;
-//아스키코드 : 대문자 65~90, 소문자 97~122
-
-int main() {
-    cin >> input;
-
-    //빈도수 체킹
-    for(int i=0;i<input.length();i++) {
-        if(input[i]<97) alpha[input[i] - 65]++; //대문자
-        else alpha[input[i] - 97]++; //소문자
-    }
-
-    int max = 0, max_indx=0;
-
-    for(int i=0;i<26;i++) {
-        if(max<alpha[i]) {
-            max=alpha[i];
-            max_indx = i;
+int main()
+{
+    int alpha[26]={0,};
+    int count=0;
+    string word;
+    
+    cin >> word;
+    
+    for(int i = 0; i<word.length(); i++)
+    {
+        if(word[i]<97)
+        {
+            alpha[word[i]-65]++;
+        }
+        else
+        {
+            alpha[word[i]-97]++;
         }
     }
-    //가장 많이 사용된 알파벳이 여러개인가?
-    for(int i=0;i<26;i++) {
-        if(max==alpha[i]) cnt++;
+    
+    int max = 0, max_index = 0;
+    
+    for(int i = 0; i < 26; i++)
+    {
+        if(max<alpha[i])
+        {
+            max = alpha[i];
+            max_index = i;
+        }
     }
-    //여러개라면 ? 출력, 아니라면 대문자로 출력
-    if(cnt>1) cout << "?";
-    else cout << (char)(max_indx+65);
+    
+    for(int i = 0; i < 26; i++)
+    {
+        if(max == alpha[i])
+            count++;
+    }
+    
+    if(count > 1)
+    {
+        cout << "?";
+    }
+    else
+    {
+        cout << (char)(max_index+65);
+    }
+
 }
