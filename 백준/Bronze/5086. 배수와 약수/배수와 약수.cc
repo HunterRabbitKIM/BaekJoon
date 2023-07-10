2e576f951bcd9e83ce2279caf71997a7e5e08e06
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-07-10 14:05:31 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+    while(true)
+    {
+        cin >> a >> b;
+        
+        if(a == 0 && b == 0)
+        {
+            break;
+        }
+        else if(a>b && a%b==0)
+        {
+            cout << "multiple" << endl;
+        }
+        else if(a<b && b%a==0)
+        {
+            cout << "factor" << endl;
+        }
+        else
+        {
+            cout << "neither" << endl;
+        }
+    }
+}
