ab9083de27266baadde8359a2758f8b6c869eb37
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-07-25 12:00:48 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    
+    cin >> n;
+    
+    cout << n*(n-1);
+}
