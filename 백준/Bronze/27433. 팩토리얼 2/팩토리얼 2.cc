0525745a821fd9d2caa9ca0a1c988170eaab2dfc
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-08-04 14:44:10 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+using namespace std;
+
+long long int fact(int n)
+{
+    if(n <= 1)
+    {
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    cout << fact(n);
+}
