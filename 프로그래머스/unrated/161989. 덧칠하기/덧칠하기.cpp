04edfeaa5cf723996fd9634e42f2b24db2def2c8
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-08-18 13:05:36 +0900

    [unrated] Title: 덧칠하기, Time: 0.17 ms, Memory: 4.98 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int paint = 0;
+    
+    for(int i = 0; i < section.size(); i++)
+    {
+        if(paint > section[i])
+        {
+            continue;
+        }
+        answer++;
+        paint = section[i]+m;
+    }
+    
+    return answer;
+}
