6ba08d7dbd9f9ce8bdec01d0c1d9225e5ff3ab6a
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-08-29 14:21:04 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    vector<int> v = {-1};
+    for(int i : ingredient)
+    {
+        if(v.back()==1&&i==2)
+        {
+            v.back() = 12;
+        }
+        else if(v.back()==12&&i==3)
+        {
+            v.back()=123;
+        }
+        else if(v.back()==123&&i==1)
+        {
+            answer++;
+            v.pop_back();
+        }
+        else v.push_back(i);
+    }
+    return answer;
+}
