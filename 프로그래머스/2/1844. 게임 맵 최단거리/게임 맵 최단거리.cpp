4b3b68f57d643eefafa1c017c55a25c36d0b7f65
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-10-28 11:54:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.20 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -2,6 +2,8 @@
 #include <queue>
 using namespace std;
 
+// Point 구조체 정의
+// x와 y좌표, distance(이동한 거리)를 저장
 struct Point {
     int x;
     int y;
@@ -9,8 +11,11 @@ struct Point {
 };
 
 int solution(vector<vector<int> > maps) {
+    // 게임 맵의 크기를 가져옴
     int n = maps.size();
     int m = maps[0].size();
+    
+    int answer = 0;
 
     vector<vector<bool> > visited(n, vector<bool>(m, false));
     queue<Point> q;
@@ -26,7 +31,7 @@ int solution(vector<vector<int> > maps) {
         q.pop();
 
         if (current.x == n - 1 && current.y == m - 1) {
-            int answer = current.distance;
+            answer = current.distance;
             return answer;
         }
 
